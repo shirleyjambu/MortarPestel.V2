9f3c9fbae7bf66e2a966e34fbb4ee2f7294a7ebb
Elbe <ashelbe08@gmail.com>
2019-02-09 18:57:28 -0500

    Added validation

--- routes/api/recipeRoutes.js
+++ routes/api/recipeRoutes.js
@@ -1,16 +1,26 @@
 const router = require("express").Router();
 const exApiController = require("./../../controller/exApiController");
 const loginController = require("./../../controller/loginController");
-const { check } = require('express-validator/check')
+const { check, validationResult } = require('express-validator/check');
 
 router.get("/quickSearch",exApiController.getQuickSearchRecipe);
 
 router
   .route("/login")
   .post([
-    check('userName').isLength({ min: 3 }),
-    check('userPassword').isLength({min: 2})
-      ],loginController.loginUser);
+    check('userName').isLength({ min: 3 }).withMessage('Invalid Username'),
+    check('userPassword').isLength({min: 6}).withMessage('Passport must be atleast 6 chars.')
+      ],function(req,res){
+        console.log("... In after check ...");
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          console.log("........... Error .........");
+          res.render("landing",{ errors: errors.array() });
+        }
+
+        loginController.loginUser;
+      }
+  );
 
       
 
