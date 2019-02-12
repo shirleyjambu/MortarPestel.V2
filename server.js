5cc6584101bcb1d8538fcacca258887272e1f28a
da Vinci <brucewilson1103@gmail.com>
2019-02-12 11:03:52 -0500

    commit 2

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const routes = require("./routes");
 const exphbs = require("express-handlebars");
-
 var session = require("express-session");
 
 var app = express();
@@ -10,29 +9,27 @@ var PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+var passport = require("./config/passport");
 
 app.engine("hbs", exphbs(
-  {extname:"hbs",
-   defaultLayout: "main",
-   layoutsDir: __dirname + '/views/layouts/',
-   partialsDir: __dirname + '/views/partials/'}
+ {extname:"hbs",
+  defaultLayout: "main",
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'}
 ));
 
-
-
-app.set("view engine","hbs");
-app.use(express.static("public"));
-app.use(expressSession({secret: 'max', saveUninitialized:false , resave:false}));
-
-app.use(session({ 
-  secret: "keyboard cat", 
-  resave: true, 
-  saveUninitialized: true }));
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.set("view engine","hbs");
+
+app.use(express.static("public"));
+app.use(expressSession({secret: 'max', saveUninitialized:false , resave:false}));
 
 app.use("/",routes);
 
 app.listen(PORT, function() {
-  console.log("MortarPestel.V2 listening on PORT " + PORT);
+ console.log("MortarPestel.V2 listening on PORT " + PORT);
 });
+
