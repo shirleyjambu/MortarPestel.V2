997e8d6d229301a2177a8554e26c1f5db345b15e
ShaBree Quinones <shabreeq@gmail.com>
2019-02-16 23:04:01 -0500

    saving work

--- models/category.js
+++ models/category.js
@@ -1,25 +1,24 @@
-const Sequelize =require("sequelize");
+const Sequelize = require("sequelize");
 
-module.exports = function(sequelize, DataTypes) {
- var Category = sequelize.define("Category", {
-   cuisine_name: {
-     type: DataTypes.STRING,
-     allowNull: false
-   },
-   createdAt:{
-     type:DataTypes.DATE,
-     allowNull : true,
-     defaultValue: Sequelize.NOW
-   },
-   updatedAt:{
-     type: DataTypes.DATE,
-     allowNull: true,
-     defaultValue: Sequelize.NOW,
-   }
+module.exports = function (sequelize, DataTypes) {
+  var Category = sequelize.define("Category", {
+    cuisine_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.NOW,
+    }
 
- },
- {
-   timestamps: true
- });
- return Category;
+  }, {
+    timestamps: true
+  });
+  return Category;
 };
--- models/cuisine.js
+++ models/cuisine.js
@@ -1,25 +1,24 @@
-const Sequelize =require("sequelize");
+const Sequelize = require("sequelize");
 
-module.exports = function(sequelize, DataTypes) {
- var Cuisines = sequelize.define("Cuisines", {
-   cuisine_name: {
-     type: DataTypes.STRING,
-     allowNull: false
-   },
-   createdAt:{
-     type:DataTypes.DATE,
-     allowNull : true,
-     defaultValue: Sequelize.NOW
-   },
-   updatedAt:{
-     type: DataTypes.DATE,
-     allowNull: true,
-     defaultValue: Sequelize.NOW,
-   }
+module.exports = function (sequelize, DataTypes) {
+  var Cuisines = sequelize.define("Cuisines", {
+    cuisine_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.NOW,
+    }
 
- },
- {
-   timestamps: true
- });
- return Cuisines;
+  }, {
+    timestamps: true
+  });
+  return Cuisines;
 };
